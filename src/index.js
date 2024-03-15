037aff9e3d1e63a21b400c7bf4510a279f83479e
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-15 12:49:07 -0700

    complete fetch on demand with forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+
+    // find second child element of form, which contains the user input:
+    inputValue = event.target.children[1].value;
+  });
+
+  // fetch request
+  fetch("http://localhost:3000/movies/${input.value}")
+    .then((response) => response.json())
+    .then((data) => {
+      // display title and summary of movie id input by user
+      const title = document.querySelector("section#movieDetails h4");
+      title.innerHTML = data.title;
+      const summary = document.querySelector("section#movieDetails p");
+      summary.innerHTML = data.summary;
+    });
+};
+
+document.addEventListener("DOMContentLoaded", init);
